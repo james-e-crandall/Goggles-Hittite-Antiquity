91dbf92916df98bdfc8d0e9512ac8483cd5026ae
james-e-crandall <james.e.crandall@gmail.com>
2025-05-06 20:25:45 -0500

    visual remap, unit replaces

---
+++ ui/game.js
@@ -0,0 +1,29 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_GOLDEN_SPEARMAN': { //your unit type
+        unitType: 'UNIT_SPEARMAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_GOLDEN_SPEARMAN_2': { //your unit type
+        unitType: 'UNIT_SPEARMAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_GOLDEN_SPEARMAN_3': { //your unit type
+        unitType: 'UNIT_SPEARMAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_GOLDEN_CHARIOT': { //your unit type
+        unitType: 'UNIT_CHARIOT',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_GOLDEN_CHARIOT_2': { //your unit type
+        unitType: 'UNIT_CHARIOT',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+}
+
+
+
+
+
+
