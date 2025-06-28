bc7667d596875ebd26ac1d2fa121bba5e49087a1
james-e-crandall <james.e.crandall@gmail.com>
2025-06-27 21:56:10 -0500

    JEC

--- ui/game.js
+++ ui/game.js
@@ -1,26 +1,26 @@
 window.UnitPortraitVisualRemaps = {
     ...(window.UnitPortraitVisualRemaps || {}),
-    'UNIT_GOLDEN_SPEARMAN': { //your unit type
+    'UNIT_GOLDEN_SPEARMAN_JEC': { //your unit type
         unitType: 'UNIT_SPEARMAN',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_GOLDEN_SPEARMAN_2': { //your unit type
+    'UNIT_GOLDEN_SPEARMAN_JEC_2': { //your unit type
         unitType: 'UNIT_SPEARMAN',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_GOLDEN_SPEARMAN_3': { //your unit type
+    'UNIT_GOLDEN_SPEARMAN_JEC_3': { //your unit type
         unitType: 'UNIT_SPEARMAN',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_GOLDEN_CHARIOT': { //your unit type
+    'UNIT_GOLDEN_CHARIOT_JEC': { //your unit type
         unitType: 'UNIT_CHARIOT',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_GOLDEN_CHARIOT_2': { //your unit type
+    'UNIT_GOLDEN_CHARIOT_JEC_2': { //your unit type
         unitType: 'UNIT_CHARIOT',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_GAL_MESEDI': { //your unit type
+    'UNIT_GAL_MESEDI_JEC': { //your unit type
         unitType: 'UNIT_ARMY_COMMANDER',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
